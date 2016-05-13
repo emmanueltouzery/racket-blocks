fc158a0cb4c452c2cfb48658bf6176b318931d0e
Emmanuel Touzery <etouzery@gmail.com>
2016-05-13 22:56:16 +0200

    at least primitive testing of piece-move-x.

--- private/test/game-logic.rkt
+++ private/test/game-logic.rkt
@@ -3,7 +3,12 @@
 (require rackunit)
 
 (require "../../drawing.rkt")
-(require/expose "../../game-logic.rkt" (piece-depths cur-piece-state board-get-item))
+(require/expose "../../game-logic.rkt"
+                (piece-depths
+                 cur-piece-state
+                 board-get-item
+                 game-state
+                 piece-move-x))
 
 (check-equal?
  (piece-depths (cur-piece-state (first pieces) #f 0 #f))
@@ -16,3 +21,39 @@
 (check-equal? (board-get-item empty 0 0) #f)
 (check-equal? (board-get-item test-board 0 0) "yellow")
 (check-equal? (board-get-item test-board 3 1) "blue")
+
+(define (get-piece-at x-tiles y-pixels)
+  (cur-piece-state
+    (piece "red" '((0 0)))
+    (void)
+    x-tiles y-pixels))
+
+(define (test-game-state x-tiles y-pixels board)
+  (game-state
+       #f (get-piece-at x-tiles y-pixels)
+       board (void)))
+
+;; TODO add in a couple of good test cases
+;; for moving left/right
+(define (check-move-x msg in-game-state move expected-game-state)
+  (check-equal?
+   expected-game-state
+   (piece-move-x move in-game-state) msg))
+
+(define y-board-bottom
+  (* (- tile-size 1) board-height-tiles))
+
+(check-move-x "move left empty board"
+ (test-game-state 5 4 '())
+ -1
+ (test-game-state 4 4 '()))
+
+(check-move-x "move left already on the left edge"
+ (test-game-state 0 4 '())
+ -1
+ (test-game-state 0 4 '()))
+
+(check-move-x "move left cell occupied"
+ (test-game-state 3 y-board-bottom '((#f #f "red" #f)))
+ -1
+ (test-game-state 3 y-board-bottom '((#f #f "red" #f))))
